8a9ed2c5b4ae0d55fc107412534532c68e2a2b6d
azamora96 <alec.zamora@western.edu>
2025-02-24 13:21:46 -0700

    flask file added

--- Code/flask_app.py
+++ Code/flask_app.py
@@ -1,13 +1,9 @@
 
-# A very simple Flask Hello World app for you to get started with...
+from flask import Flask
 
-from flask import Flask, render_template
-
-app = Flask(__name__, static_url_path="/static")
+app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('hw13_home.html')
-
-
+def hello_world():
+    return 'Hello from Flask!'
 
