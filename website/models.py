a6c5fc7ce38637b250c93f03b830d50c3475c3e2
azamora96 <alec.zamora@western.edu>
2025-02-24 19:16:34 -0700

    database created - table created + populated

---
+++ website/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class Products(db.Model):
+    primary_id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(100))
+    name = db.Column(db.String(50))
+    tefap = db.Column(db.String(5))
+    best_by = db.Column(db.Date)
+    expiration = db.Column(db.Date)
+    location = db.Column(db.String(50))
+    quantity = db.Column(db.Integer)
+    date_arrived = db.Column(db.Date)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,33 @@
 from flask import Blueprint, render_template
+from . import db
+from .models import Products
+from datetime import datetime
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    date_format = "%Y-%m-%d"
+    best_by_date = datetime.strptime("2025-02-24", date_format).date()
+    expiration_date = datetime.strptime("2025-02-24", date_format).date()
+    date_arrived_date = datetime.strptime("2025-02-24", date_format).date()
+
+    test_product = Products.query.filter_by(name="Test Product").first()
+
+    if not test_product:
+        test_product = Products(
+            image="test_image.png",
+            name="Test Product",
+            tefap="Yes",
+            best_by=best_by_date,
+            expiration=expiration_date,
+            location="Test Location",
+            quantity=100,
+            date_arrived=date_arrived_date,
+        )
+        db.session.add(test_product)
+        db.session.commit()
+
+    all_products = Products.query.all()
+
+    return render_template("home.html", results=all_products)
