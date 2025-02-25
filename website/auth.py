4b6368552c2eca99ab38961afcd356d01d9e869a
Van Mikkelsen <vanlfmikk02@gmail.com>
2025-02-25 15:56:11 -0700

    ?

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,29 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+
+#from . import db
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password == password:
+                flash("Succesful Log in")
+                return redirect(url_for('view.home'))
+            else:
+                flash("Incorrect Password")
+        else:
+            flash("Incorrect User")
+
+
+
     return render_template("login.html", boolean=True)
 
 @auth.route("/logout")
--- website/models.py
+++ website/models.py
@@ -1,4 +1,5 @@
 from . import db
+from flask_login import UserMixin
 
 class Products(db.Model):
     primary_id = db.Column(db.Integer, primary_key=True)
@@ -10,3 +11,8 @@ class Products(db.Model):
     location = db.Column(db.String(50))
     quantity = db.Column(db.Integer)
     date_arrived = db.Column(db.Date)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(150), unique = True)
+    password = db.Column(db.String(150))
