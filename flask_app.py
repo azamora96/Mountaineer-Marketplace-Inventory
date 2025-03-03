7ec04e856ba835a9382f34098930ac610218bada
Van Mikkelsen <vanlfmikk02@gmail.com>
2025-03-02 21:16:15 -0700

    Begone statement
    I hate github

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,6 @@ from website import create_app
 
 app = create_app()
 
-# Comment to make main realize login_real isn't merged
-stupid = false
-if(stupid):
-    print(stupid)
 
 if __name__ == '__main__':
     app.run(debug=True)
