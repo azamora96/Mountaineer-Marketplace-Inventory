7453fc3019b3a1c94053b6e3937e7aa4b87b13a9
Van Mikkelsen <vanlfmikk02@gmail.com>
2025-03-03 13:02:52 -0700

    Login Stuff

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -18,5 +19,15 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+
+    from .models import User 
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
